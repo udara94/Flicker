882ef3c7fb3e31845961b5604b433bf147f63ff7
Udara Abeythilake <udara.i@eyepax.com>
2019-04-10 13:49:01 +0530

    react-1

--- React Boilerplate/src/index.js
+++ React Boilerplate/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Home from "./home";
 
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Eyepax JavaScript Hackathon</h1>
+    <div >
+      <Home/>
     </div>
   );
 }
